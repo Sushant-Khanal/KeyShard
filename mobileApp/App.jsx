ddce8d49dc2d0c5c64552314ef85f392b7ee2bb8
Bhusal-Ravi <ravidonbhusal@gmail.com>
2025-11-24 20:01:13 +0545

    ReactNative Initialization Expo for runtime environment NativeWind for styling Babble

---
+++ mobileApp/App.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Home from './app/Home'
+import './global.css'
+
+const App = () => {
+    return (
+        <View>
+            <Home />
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ mobileApp/app/Home.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import '../global.css'
+
+
+const Home = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-red-500 text-lg mt-10">Homeeee</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
